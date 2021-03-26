a487b8b25d0e71e539b22718548951b9e8839b68
Eugene Kushnir <kushnir.fort@gmail.com>
2021-03-26 10:24:11 +0200

    connect Webpack and split js file into modules

---
+++ js/modules/timer.js
@@ -0,0 +1,59 @@
+function timer () {
+    const deadTime = '2021-03-27T08:59';
+
+    function getTimeRemainining(endTime){
+        const t = Date.parse(endTime) - Date.parse(new Date()),
+              days = Math.floor(t / (1000*60*60*24)),
+              hours = Math.floor((t / (1000*60*60) % 24)),
+              minutes = Math.floor((t / (1000*60) % 60)),
+              second = Math.floor((t / 1000 % 60));
+
+        return {
+            'total': t,
+            'days': days,
+            'hours': hours,
+            'minutes': minutes,
+            'second': second
+        };   
+    }
+
+        function getZero (num){
+            if(num >= 0 && num < 10){
+                return `0${num}`;
+            } else {
+                return num;
+            }
+        }
+    function setClock(selector, endTime) {
+        const timer = document.querySelector(selector),
+            days = timer.querySelector('#days'),
+            hours = timer.querySelector('#hours'),
+            minutes = timer.querySelector('#minutes'),
+            second = timer.querySelector('#seconds'),
+            timeInterval = setInterval(updateClock, 1000);
+
+            updateClock();
+
+        function updateClock(){
+            const t = getTimeRemainining(endTime);
+
+            days.innerHTML = getZero(t.days);
+            hours.innerHTML = getZero(t.hours) ;
+            minutes.innerHTML = getZero(t.minutes);
+            second.innerHTML = getZero(t.second);
+
+
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+                days.innerHTML = '-';
+                hours.innerHTML = '-';
+                minutes.innerHTML = '-';
+                second.innerHTML = '-';
+            }
+        }    
+    }
+
+    setClock('.timer', deadTime);
+}
+
+module.exports = timer;
